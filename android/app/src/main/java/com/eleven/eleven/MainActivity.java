7aec420c6c1dab1a0fec27046ecad267db778c9c
adreanhr078-crypto <adreanhr078@gmail.com>
2026-08-06 16:49:13 +0300

    1

--- android/app/src/main/java/com/eleven/eleven/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.eleven.eleven;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function App() {
-  return (
-    <div>
-      <h1>11:11 - Futuristic Eleven Eleven</h1>
-    </div>
-  );
-}
